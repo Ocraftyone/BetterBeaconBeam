d18927e39a78753321bd26615e51753c4863c17a
Ocraftyone <Ocraftyone@users.noreply.github.com>
2022-08-31 00:10:09 -0400

    fix issue where it didn't work in production

--- src/main/java/com/ocraftyone/betterbeaconbeam/mixin/BeaconRendererMixin.java
+++ src/main/java/com/ocraftyone/betterbeaconbeam/mixin/BeaconRendererMixin.java
@@ -3,10 +3,15 @@ package com.ocraftyone.betterbeaconbeam.mixin;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.blockentity.BeaconRenderer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(BeaconRenderer.class)
 public abstract class BeaconRendererMixin {
-    
+    /**
+     * @author Ocraftyone
+     * @reason Tie beacon beam to render distance
+     */
+    @Overwrite
     public int getViewDistance() {
         return Minecraft.getInstance().options.renderDistance().get() * 16;
     }
